3ef56a98a30016d0231195350ed67002eb76c7a9
Oscar Martinez <maroscar32@gmail.com>
2023-01-29 14:21:05 -0600

    Funcionalidad de seccion de contacto

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const form = document.getElementById ('form');
+const enviarCorreo = document.getElementById('correo');
 
+
+form.addEventListener('submit',function(event){
+    event.preventDefault(false);
+    const datosForm = new FormData(this)
+    enviarCorreo.setAttribute('href',`mailto:1998.omartinez@gmail.com?subject=${datosForm.get('name')} - ${datosForm.get('subject')} - ${datosForm.get('email')}&body=${datosForm.get('message')}`)
+    enviarCorreo.click()
+})
